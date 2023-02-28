0fef81e4dc2d1aec2dca47d62a29aa09281c475c
Dennis Jeminal <dennisjeminal8@gmail.com>
2023-02-27 20:24:13 -0500

    make it better

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a landing page for a imaginary travelling agency (WTA - World Travelling Aggency)
+
+Built with
+1.HTML
+2.CSS
+3.Javascript
--- app.js
+++ app.js
@@ -20,6 +20,20 @@ function openCloseMobileNavDrawer(){
     }
 }
 
-window.addEventListener('resize', ()=>{
-    // console.log(window.innerWidth)
-})
+
+// using the observe element fn to animate elements on scroll
+observeElement(document.querySelector('footer'), cb)
+
+// this observes the dom element passed to it and runs the func passed to it.
+function observeElement(domElement, callbackFn){
+    let options = {root:null, threshold:0.5}
+
+    const reveal = (entries, observer)=>{
+        let isIntersecting = entries[0].isIntersecting
+        if(isIntersecting){callbackFn()}
+        return null
+    }
+
+    let observer = new IntersectionObserver(reveal, options)
+    observer.observe(domElement)
+}
