39819eff79aec5613d191bf3ef4d166191b6d85a
Dennis Jeminal <dennisjeminal8@gmail.com>
2023-03-01 12:39:43 -0500

    added animations

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mobileNav = document.querySelector('[mobile-nav]')
 const mobileNavIcon = document.querySelector('[mobile-nav-icon]')
+const mobileNavList = document.querySelector('[mobile-nav-list]')
 const mobileNavEmpty = document.querySelector('[mobile-nav-empty]')
 
 mobileNavIcon.addEventListener('click', openCloseMobileNavDrawer)
@@ -9,20 +10,25 @@ mobileNavEmpty.addEventListener('click', openCloseMobileNavDrawer)
 function openCloseMobileNavDrawer(){
     // check if drawer is open base on the classes
     const isOpen = mobileNav.className.includes('mobile-drawer-open')
-    console.log(isOpen)
     if(isOpen){ //close the drawer
-        mobileNav.classList.remove("mobile-drawer-open")
-        mobileNav.classList.add("mobile-drawer-close")
+        gsap.to(mobileNavList, {x:-500})
+        gsap.to(mobileNavEmpty, {opacity:0})
+        setTimeout(()=>{
+            mobileNav.classList.remove("mobile-drawer-open")
+            mobileNav.classList.add("mobile-drawer-close")
+        },200)
     }
     else{ //open the drawer
         mobileNav.classList.remove("mobile-drawer-close")
         mobileNav.classList.add("mobile-drawer-open")
+        gsap.to(mobileNavList, {x:0})
+        gsap.to(mobileNavEmpty, {opacity:1})
     }
 }
 
 
 // using the observe element fn to animate elements on scroll
-observeElement(document.querySelector('footer'), cb)
+// observeElement(document.querySelector('footer'), cb)
 
 // this observes the dom element passed to it and runs the func passed to it.
 function observeElement(domElement, callbackFn){
@@ -30,10 +36,63 @@ function observeElement(domElement, callbackFn){
 
     const reveal = (entries, observer)=>{
         let isIntersecting = entries[0].isIntersecting
-        if(isIntersecting){callbackFn()}
-        return null
+        if(isIntersecting){
+            callbackFn()
+            observer.unobserve(entries[0].target)
+        }
     }
 
     let observer = new IntersectionObserver(reveal, options)
     observer.observe(domElement)
 }
+
+// ANIMATIONS
+
+// hero texts animattions
+const heroTextsTm = gsap.timeline({defaults:{duration:.5}})
+const heroText = document.querySelector('[hero-text]')
+const heroSubText = document.querySelector('[hero-sub-text]')
+
+heroTextsTm.fromTo(heroText, {opacity:0, y:-50},{opacity:1, y:0,})
+heroTextsTm.fromTo(heroSubText, {opacity:0, y:50},{opacity:1, y:0,})
+
+
+// plcaces animations
+const placesTm = gsap.timeline()
+const placesContainer = document.querySelector('[places-container]')
+const places = document.querySelectorAll('[place]')
+
+function animatePlaces(){
+    places.forEach((place) => {
+        placesTm.to(place, {opacity:1, scale:1})
+    })
+}
+
+// observe the parent element entry and trigger its children animation
+observeElement(placesContainer, animatePlaces)
+
+// about us team animations
+const teamPeopleTm = gsap.timeline()
+const aboutUsTeam = document.querySelector('[about-us-team]')
+const teamPeople = document.querySelectorAll('[team-person]')
+
+function animateTeamPeople(){
+    teamPeople.forEach((teamPerson) => {
+        teamPeopleTm.to(teamPerson, {x:0, opacity:1})
+    })
+}
+
+observeElement(aboutUsTeam, animateTeamPeople)
+
+// agency text animations
+const agencyTm = gsap.timeline({defaults:{opacity:0}})
+const agencyTexts = document.querySelector('[agency-texts]')
+const agencyMainText = document.querySelector('[agency-main-text]')
+const agencySubText = document.querySelector('[agency-sub-text]')
+
+function animateTexts(){
+    agencyTm.fromTo(agencyMainText, {y:20}, {opacity:1, y:0})
+    agencyTm.fromTo(agencySubText, {y:-20}, {opacity:1, y:0})
+}
+
+observeElement(agencyTexts, animateTexts)
