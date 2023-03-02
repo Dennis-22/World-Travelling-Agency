768f9b86de5bb3f8f6807168a2fa167a7e3f4c2b
Dennis Jeminal <dennisjeminal8@gmail.com>
2023-03-02 11:50:06 -0500

    make it better

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ function openCloseMobileNavDrawer(){
     // check if drawer is open base on the classes
     const isOpen = mobileNav.className.includes('mobile-drawer-open')
     if(isOpen){ //close the drawer
+        // animate the drawer off and remove from dom
         gsap.to(mobileNavList, {x:-500})
         gsap.to(mobileNavEmpty, {opacity:0})
         setTimeout(()=>{
@@ -19,6 +20,7 @@ function openCloseMobileNavDrawer(){
         },200)
     }
     else{ //open the drawer
+        // bring the drawer to dom and animate it to view
         mobileNav.classList.remove("mobile-drawer-close")
         mobileNav.classList.add("mobile-drawer-open")
         gsap.to(mobileNavList, {x:0})
@@ -31,8 +33,8 @@ function openCloseMobileNavDrawer(){
 // observeElement(document.querySelector('footer'), cb)
 
 // this observes the dom element passed to it and runs the func passed to it.
-function observeElement(domElement, callbackFn){
-    let options = {root:null, threshold:0.5}
+function observeElement(domElement, callbackFn, threshold=0.5){
+    let options = {root:null, threshold}
 
     const reveal = (entries, observer)=>{
         let isIntersecting = entries[0].isIntersecting
@@ -48,7 +50,7 @@ function observeElement(domElement, callbackFn){
 
 // ANIMATIONS
 
-// hero texts animattions
+// hero texts animations
 const heroTextsTm = gsap.timeline({defaults:{duration:.5}})
 const heroText = document.querySelector('[hero-text]')
 const heroSubText = document.querySelector('[hero-sub-text]')
@@ -57,8 +59,8 @@ heroTextsTm.fromTo(heroText, {opacity:0, y:-50},{opacity:1, y:0,})
 heroTextsTm.fromTo(heroSubText, {opacity:0, y:50},{opacity:1, y:0,})
 
 
-// plcaces animations
-const placesTm = gsap.timeline()
+// places animations
+const placesTm = gsap.timeline({defaults:{duration:.5}})
 const placesContainer = document.querySelector('[places-container]')
 const places = document.querySelectorAll('[place]')
 
@@ -71,28 +73,26 @@ function animatePlaces(){
 // observe the parent element entry and trigger its children animation
 observeElement(placesContainer, animatePlaces)
 
-// about us team animations
-const teamPeopleTm = gsap.timeline()
-const aboutUsTeam = document.querySelector('[about-us-team]')
-const teamPeople = document.querySelectorAll('[team-person]')
 
-function animateTeamPeople(){
-    teamPeople.forEach((teamPerson) => {
-        teamPeopleTm.to(teamPerson, {x:0, opacity:1})
+// destinations animations
+const destinations = document.querySelectorAll('[destination]')
+const destinationTm = gsap.timeline({defaults:{duration:.4}})
+
+destinations.forEach((destination)=>{
+    observeElement(destination, ()=>{
+        destinationTm.to(destination, {opacity:1, y:0})
     })
-}
-
-observeElement(aboutUsTeam, animateTeamPeople)
+})
 
 // agency text animations
-const agencyTm = gsap.timeline({defaults:{opacity:0}})
+const agencyTm = gsap.timeline()
 const agencyTexts = document.querySelector('[agency-texts]')
 const agencyMainText = document.querySelector('[agency-main-text]')
 const agencySubText = document.querySelector('[agency-sub-text]')
 
 function animateTexts(){
-    agencyTm.fromTo(agencyMainText, {y:20}, {opacity:1, y:0})
-    agencyTm.fromTo(agencySubText, {y:-20}, {opacity:1, y:0})
+    agencyTm.to(agencyMainText, {opacity:1, y:0})
+    agencyTm.to(agencySubText, {opacity:1, y:0})
 }
 
 observeElement(agencyTexts, animateTexts)
